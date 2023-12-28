e98606fa5dc3404922c56073af0a5a7256454781
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-28 13:49:34 +0530

    Lastname validator

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,9 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Raghav");
-        System.out.println(user);
-        User user1 = new User("raghav");
-        System.out.println(user1);
+        User user = new User("Raghav", "Patidar");
+        User user1 = new User("raghav", "Patidar");
     }
 }
--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -5,19 +5,37 @@ import java.util.regex.Pattern;
 
 public class User {
     String firstName;
+    String lastName;
 
-    User(String firstName) {
-        if (!validate(firstName)) {
-            throw new IllegalArgumentException("First latter should be Capital with minimum 3 latters ");
+    User(String firstName, String lastName) {
+        if (!validate(firstName, lastName)) {
+            throw new IllegalArgumentException();
         }
         this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("User created successfully : " + this.toString());
     }
 
-    private boolean validate(String firstName) {
+    private boolean validate(String firstName, String lastName) {
         Pattern pattern = Pattern.compile("^[A-Z].{2,}$");
         boolean matcher = pattern.matcher(firstName).matches();
+        if (!matcher) {
+            System.out.println("First latter should be Capital with minimum 3 latters ");
+            return false;
+        }
+
+        matcher = pattern.matcher(lastName).matches();
+        if (!matcher) {
+            System.out.println("Last latter should be Capital with minimum 3 latters ");
+            return false;
+        }
         System.out.println(matcher);
-        return matcher;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + ", lastName: " + lastName;
     }
 
 }
